8f3055d44916b9f7d4e4f187eb5871d7a9f5aa2e
Pham Huy Hoang <hoang.phamhuy@haravan.com>
2019-05-14 15:53:32 +0700

    .

--- backend/blockchain/module/contract/model.js
+++ backend/blockchain/module/contract/model.js
@@ -4,9 +4,9 @@ const Plan = mongoose.model('plan');
 const Contract = mongoose.model('contract');
 
 module.exports.FindByLicensePlate = async licensePlate => {
-	return await Contract.find({licensePlate: licensePlate});
+	return await Contract.find({licensePlate: licensePlate}).populate('plan');
 };
 
 module.exports.FindByCompany = async company => {
-	return await Plan.find({company: company}).populate('contracts').select('contracts');
+	return await Plan.find({company: company}).populate('contracts');
 };
