220510176329167fd69d138da554e1f649834a44
Pham Huy Hoang <hoang.phamhuy@haravan.com>
2019-05-31 16:37:36 +0700

    .

--- backend/shop/module/comment/business/comment-business.js
+++ backend/shop/module/comment/business/comment-business.js
@@ -1,7 +1,7 @@
 const Comment = require('../model/comment-model');
 
 module.exports.getAllComments = async () => {
-	return await Comment.find({}).sort({id: -1}).lean();
+	return await Comment.find({}).populate({path: 'user', select: 'username'}).sort({id: -1}).lean();
 };
 
 module.exports.postComment = async (user, content) => {
